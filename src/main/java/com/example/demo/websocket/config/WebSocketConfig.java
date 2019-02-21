2ee9d7805c09d6c695b10a57b46ab49e61380baf
lvshaokang <lvshaokang@hotmail.com>
2019-02-21 18:10:53 +0800

    stomp-token 集成

--- src/main/java/com/example/demo/websocket/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/websocket/config/WebSocketConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.websocket.config;
 
+import com.example.demo.websocket.interceptor.WebSocketChannelInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -20,7 +22,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
 
-        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("*")
+                .withSockJS();
 
         // 点对点
         registry.addEndpoint("/ws/point").withSockJS();
@@ -38,8 +41,17 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic", "/user");
 
         // 区分websocket和普通请求
-//        registry.setApplicationDestinationPrefixes("/app");
+        registry.setApplicationDestinationPrefixes("/app");
+
+        registry.setUserDestinationPrefix("/user");
     }
 
+    /**
+     * 拦截器配置
+     */
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new WebSocketChannelInterceptor());
 
+    }
 }
